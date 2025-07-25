f05ffb12ad05927da01fa098c56e891c68158e1f
umon <usmankpc72@gmail.com>
2025-07-24 20:34:08 -0500

    moved addToCart function to cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,24 @@
 export const cart = [];
+
+export function addToCart(productId) {
+  const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+  const selectedQuantity = Number(quantitySelector.value);
+
+  let matchingItem;
+
+  cart.forEach((cartItem) => {
+    if (productId === cartItem.productId) {
+      matchingItem = cartItem;
+      return;
+    }
+  });
+
+  if (matchingItem) {
+    matchingItem.quantity += selectedQuantity;
+  } else {
+    cart.push({
+      productId,
+      quantity: selectedQuantity,
+    });
+  }
+}
