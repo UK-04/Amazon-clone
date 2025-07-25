e83025d6d903d68de053188d3ec33f44e2bf76f7
umon <usmankpc72@gmail.com>
2025-07-24 20:22:28 -0500

    use modules and import cart from cart.js

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,3 +1,4 @@
+import {cart} from '../data/cart.js';
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -25,7 +26,7 @@ products.forEach((product) => {
       </div>
 
       <div class="product-quantity-container">
-        <select>
+        <select class="js-quantity-selector-${product.id}">
           <option selected value="1">1</option>
           <option value="2">2</option>
           <option value="3">3</option>
@@ -60,6 +61,9 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     const productId = button.dataset.productId;
     productId;
 
+    const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+    const selectedQuantity = Number(quantitySelector.value);
+
     let matchingItem;
 
     cart.forEach((item) => {
@@ -70,14 +74,13 @@ document.querySelectorAll('.js-add-to-cart').forEach((button) => {
     });
 
     if (matchingItem) {
-      matchingItem.quantity++;
+      matchingItem.quantity += selectedQuantity;
     } else {
       cart.push({
         productId,
-        quantity: 1,
+        quantity: selectedQuantity,
       });
     }
-
     let cartQuantity = 0;
 
     cart.forEach((item) => {
