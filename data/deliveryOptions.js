e8795ecc83278015012bd21d3290d98e70fd0ccd
umon <usmankpc72@gmail.com>
2025-07-30 22:36:29 -0500

    paymentSummary now calculates everything needed

--- data/deliveryOptions.js
+++ data/deliveryOptions.js
@@ -13,3 +13,15 @@ export const deliveryOptions = [
     priceCents: 999
   }
 ];
+
+export function getDeliveryOption(deliveryOptionId) {
+  let selectedDeliveryOption;
+
+  deliveryOptions.forEach((option) => {
+    if (option.id === deliveryOptionId) {
+      selectedDeliveryOption = option;
+    }
+  });
+
+  return selectedDeliveryOption || deliveryOptions[0];
+}
--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,10 +1,10 @@
 import {cart, removeFromCart, updateDeliveryOption} from '../../data/cart.js';
-import {products} from '../../data/products.js';
+import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
-import {deliveryOptions} from '../../data/deliveryOptions.js'
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
 
-function renderOrderSummary() {
+export function renderOrderSummary() {
 
   let cartSummaryHTML = '';
   let totalQuantity = 0; // total items in cart
@@ -14,23 +14,11 @@ function renderOrderSummary() {
 
     totalQuantity += cartItem.quantity;
 
-    let matchingProduct;
-
-    products.forEach((product) => {
-      if(productId === product.id) {
-        matchingProduct = product;
-      }
-    });
+    const matchingProduct = getProduct(productId);
 
     const deliveryOptionId = cartItem.deliveryOptionId;
 
-    let selectedDeliveryOption;
-
-    deliveryOptions.forEach((option) => {
-      if (option.id === deliveryOptionId) {
-        selectedDeliveryOption = option;
-      }
-    });
+    const selectedDeliveryOption = getDeliveryOption(deliveryOptionId);
 
     const today = dayjs();
     const deliveryDate = today.add(selectedDeliveryOption.deliveryDays, 'days');
