7a87ee23cd31ba7ca41833f130bb337976f41602
umon <usmankpc72@gmail.com>
2025-07-31 19:40:52 -0500

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,9 +3,9 @@ import {products, getProduct} from '../../data/products.js';
 import {formatCurrency} from '../utils/money.js';
 import dayjs from 'https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js';
 import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOptions.js'
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary() {
-
   let cartSummaryHTML = '';
   let totalQuantity = 0; // total items in cart
 
@@ -115,6 +115,7 @@ export function renderOrderSummary() {
       link.addEventListener('click', () => {
         const productId = link.dataset.productId;
         removeFromCart(productId);
+        renderPaymentSummary();
 
         const container = document.querySelector(`.js-cart-item-container-${productId}`);
         container.remove();
@@ -128,9 +129,9 @@ export function renderOrderSummary() {
         const {productId , deliveryOptionId} = element.dataset;
         updateDeliveryOption(productId, deliveryOptionId);
         renderOrderSummary();
+        renderPaymentSummary();
       });
     });
 }
 
-renderOrderSummary();
 
